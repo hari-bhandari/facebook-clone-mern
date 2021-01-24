cfea476ecc91491910ee7856f3114199dd844b65
hari-bhandari <2012bhandari.ha@gmail.com>
2021-01-24 07:02:35 +0000

    manipulated css by a bit

--- src/Components/Feed/Post/Post.CSS.ts
+++ src/Components/Feed/Post/Post.CSS.ts
@@ -4,7 +4,7 @@ export const PostContainer=styled.div`
   width: 100%;
   margin-top: 15px;
   border-radius: 15px;
-  background-color: #fff;
+  background-color: #242526;
   box-shadow: 0px 5px 7px -7px rgba(0, 0, 0, 0.75);
 `
 export const PostTop=styled.div`
@@ -14,9 +14,16 @@ export const PostTop=styled.div`
   padding: 15px;
   h3 {
     font-size: medium;
+    color: white;
+
   }
   .post__topInfo > h3 {
     font-size: medium;
+    color: white;
+  }
+  p{
+    color: white;
+
   }
 
 `
@@ -25,6 +32,8 @@ export const PostBottom=styled.div`
   margin-top: 10px;
   margin-bottom: 10px;
   padding: 15px 25px;
+  color: white;
+
 `
 export const Options=styled.div`
   padding-top: 10px;
@@ -48,7 +57,7 @@ export const Options=styled.div`
   }
 
   .post__option:hover {
-    background-color: #eff2f5;
+    background-color: #45474a;
     border-radius: 10px;
   }
 
